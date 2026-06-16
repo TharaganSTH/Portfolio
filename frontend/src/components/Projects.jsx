07d343186e9b53251074a6c1fc9e165b3b0d3e91
Tharagan <tharagan05@gmail.com>
2026-06-16 16:48:56 +0530

    added project button and tags and some design improvements

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -2,22 +2,27 @@ const projects = [
   {
     title: "Disease NER & Synonym Generalization",
     desc: "Final year project focused on improving disease Named Entity Recognition using BioBERT and contrastive learning. Developed a browser extension capable of real-time biomedical text understanding.",
+    tags: ["Python", "BioBERT", "NLP", "React"],
   },
   {
     title: "Excel Sheet Format Converter",
     desc: "Developed a Java application for Rockland Distilleries that converts and standardizes Excel sheet formats using Apache POI.",
+    tags: ["Python", "BioBERT", "NLP", "React"],
   },
   {
     title: "Baby Eye Tracking System",
     desc: "Group software development project using machine learning techniques to monitor infant eye movements for early vision issue detection.",
+    tags: ["Python", "BioBERT", "NLP", "React"],
   },
   {
     title: "Raspberry Pi Sensor Data Logger",
     desc: "Built an IoT solution using Raspberry Pi Pico W and BME280 sensors to collect temperature, humidity, and pressure data.",
+    tags: ["Python", "BioBERT", "NLP", "React"],
   },
   {
     title: "Directed Graph Cycle Detection Algorithm",
     desc: "Developed a Java program to determine whether a directed graph is acyclic using sink elimination and custom graph data structures.",
+    tags: ["Python", "BioBERT", "NLP", "React"],
   },
 ];
 
@@ -32,6 +37,11 @@ function Projects() {
           <div className="project" key={project.title}>
             <h3>{project.title}</h3>
             <p>{project.desc}</p>
+            <div className="project-tags">
+                {project.tags.map((tag) => (
+                    <span key={tag}>{tag}</span>
+                ))}
+            </div>
           </div>
         ))}
       </div>
